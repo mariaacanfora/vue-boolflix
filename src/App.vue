1d93af7bb6d4ae81bb9a5d11a7f0717a60dedfcf
mariaacanfora <ma.maria.acanfora@gmail.com>
2021-11-17 18:37:12 +0100

    Milestone 2: added languages flags

--- src/App.vue
+++ src/App.vue
@@ -2,8 +2,8 @@
   <div id="app">
     <div class="container text-center">
       <div class="input-group justify-content-center py-5">
-        <input type="text" class="w-25" v-model="query" @keyup.enter="serverCall('/search/movie', query, 'movies')" placeholder="Search..."/>
-        <button class="btn btn-outline-secondary" type="button" @click="serverCall('/search/movie', query, 'movies')">Cerca!</button>
+        <input type="text" class="w-25" v-model="query" @keyup.enter="serverCall('/search/movie', query, 'movies')" placeholder="Search..."/> 
+        <button class="btn btn-outline-secondary" type="button" @click="serverCall('/search/movie', query, 'movies')">Cerca!</button> 
       </div>
 
       <ul class="list-unstyled">
@@ -11,7 +11,7 @@
           <h5>{{ movie.title }}</h5>
           <ul class="list-unstyled">
             <li>Titolo orginale: {{ movie.original_title }}</li>
-            <li>Lingua: {{ movie.original_language }}</li>
+            <li class="d-inline-block"><img alt="language's logo" :src="getLanguageFlag[movie.original_language] ? require('./assets/img/' + getLanguageMap[movie.original_language]) : require('./assets/img/default.png')" class="w-15"></li>
             <li>Voto: {{ movie.vote_average }}</li>
           </ul>
         </li>
@@ -30,9 +30,10 @@ export default {
     return {
       apiKey: "bdb91d3c68495961f770560622014e12",
       apiUrl: "https://api.themoviedb.org/3",
-      query: "",
+      query: "ciao",
       movies: [],
       series: [],
+      flags: ["en", "it", "fr", "es", "de"]
     };
   },
 
@@ -51,22 +52,54 @@ export default {
     },
   },
 
-  /* mounted() {
-    this.serverCall("/search/movie", this.query, "movies");
-    //this.serverCall("/search/tv", this.query, "series");
-  }, */
-};
+  computed: {
+    /**
+     * restituisce un oggetto le cui chiavi sono le lingue dei film apparsi nella ricerca e i valori sono stringhe tipo "language.png"
+     */
+    getLanguageMap(){
+      const languageMap = {}
+      this.movies.forEach(movie => {
+        const {original_language} = movie;
+
+        if (!languageMap[original_language]) {
+          languageMap[original_language] = original_language + ".png"
+        }
+      });
+      
+      return languageMap
+    },
+
+    /**
+     * restituisce un oggetto le chiavi sono le lingue dei film apparsi nella ricerca ed i valori sono booleani, a seconda che queste lingue siano presenti o meno nella lista delle bandiere a disposizione (array flags)
+     */
+    getLanguageFlag(){
+      //let thereIsFlag = false;
+      const objMap = {...(this.getLanguageMap)}
+      //console.log(objMap);
+
+      for (const key in objMap) {
+        if (this.flags.includes(key)) {
+          objMap[key] = true
+        } else {
+          objMap[key] = false
+        }
+      }
+      return objMap
+    }
+
+  },
+
+  /* mounted(){
+    this.serverCall('/search/movie', this.query, 'movies')
+  } */
+
+}
 </script>
 
 <style lang="scss">
 @import "~bootstrap/scss/bootstrap";
+.w-15{
+  width: 15%;
+}
 
-/* #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-} */
 </style>
