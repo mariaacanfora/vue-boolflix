6fe95918a08a8d4c2c3e3d8f0b44eda615c504e6
mariaacanfora <ma.maria.acanfora@gmail.com>
2021-11-19 13:52:41 +0100

    Milestone 4: changed methods in Card component with computed

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,12 +1,12 @@
 <template>
         <div class="col p-0 card-container text-center">
-            <img :src="getPosterImg('w342', film.poster_path)" alt="film's poster"  class="poster-img"/>
+            <img :src="getPosterImg" alt="film's poster"  class="poster-img"/>
             <div class="d-flex flex-column align-items-center overlay p-5">
                 <h1>{{film.title}}</h1> <!-- se è una serie serve film.name -->
                 <h3 v-if="film.title !== film.original_title">{{film.original_title}}</h3>
-                <img :src="getLangFlag(film.original_language)" alt="language's flag" class="w-15">
-                <span><i class="fa fa-star" aria-hidden="true" v-for="(star, i) in getVote(film.vote_average)" :key="i"></i>
-                <i class="fa fa-star-o" aria-hidden="true" v-for="(star, i) in (5 - getVote(film.vote_average))" :key="'A' + i"></i></span>
+                <img :src="getLangFlag" alt="language's flag" class="w-15">
+                <span><i class="fa fa-star" aria-hidden="true" v-for="(star, i) in getVote" :key="i"></i>
+                <i class="fa fa-star-o" aria-hidden="true" v-for="(star, i) in (5 - getVote)" :key="'A' + i"></i></span>
                 <!-- <p class="text-center">{{ film.vote_average }}</p> -->
                 <p class="text-center">{{film.overview}}</p>
             </div> 
@@ -32,27 +32,31 @@ export default {
     };
   },
 
-  methods: {
-    getPosterImg(dimension, path) {
+  computed: {
+    getPosterImg() {
+      const path = this.film.poster_path;
+
       if (path === null) {
         return require("../assets/img/default_poster.png");
       }
-      return this.imgUrl + dimension + path;
+      return this.imgUrl + 'w342' + path;
     },
 
-    getLangFlag(searchedLang){
+    getVote(){
+      const origVote = this.film.vote_average
+      const vote = origVote / 2;
+      const solidStars = Math.ceil(vote); 
+      return solidStars
+    },
+
+    getLangFlag(){
+      const searchedLang = this.film.original_language;
       if (!this.flags[searchedLang]){
         return require('../assets/img/default.png')
       }
       return require('../assets/img/' + this.flags[searchedLang])
     },
-
-    getVote(origVote){
-      const vote = origVote / 2;
-      const solidStars = Math.ceil(vote); 
-      return solidStars
-    },
-  },
+  }
 };
 </script>
 
